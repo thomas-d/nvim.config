844cbe54f9a9eea339f4678060f2b907f2b70d8c
Thomas Dedericks <tom.ded@gmail.com>
2026-03-31 10:25:29 +0200

    merge mixup

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 require 'thomasd.options'
 require 'thomasd.keymaps'
 require 'thomasd.autocommands'
-require 'thomas.options'
-require 'thomas.keymaps'
 
 -- lazy plugin manager
 local lazypath = vim.fn.stdpath 'data' .. 'lazy/lazy.nvim'
